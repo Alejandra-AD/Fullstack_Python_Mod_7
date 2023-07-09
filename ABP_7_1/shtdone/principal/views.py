c5e24b6bdf5268fd52470f3053397fc56fccba58
Alejandra C. Araya Díaz <Arayadiaz.ac@gmail.com>
2023-07-09 20:07:54 -0300

    abp_7_1

---
+++ ABP_7_1/shtdone/principal/views.py
@@ -0,0 +1,32 @@
+
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login
+from django.views.generic import TemplateView
+from .forms import LoginForm
+
+# Create your views here.
+def home(request):
+    return render(request,'tasks/home_tareas.html')
+
+class LoginUsuarioView(TemplateView):
+    template_name = 'registration/login.html'
+
+    def get(self, request, *args, **kwargs):
+        form = LoginForm()
+        return render(request, self.template_name, { "form": form })
+
+    def post(self, request, *args, **kwargs):
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect('home')
+            form.add_error('username', 'Credenciales incorrectas')
+            return render(request, self.template_name, { "form": form })
+        else:
+            return render(request, self.template_name, { "form": form })
--- Sprint_m7/shtdone/principal/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Etiqueta,Priority
-# Register your models here.
-
-admin.site.register(Etiqueta)
-admin.site.register(Priority)
